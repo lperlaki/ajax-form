9e8f43df6d41deed5e8a950c4cba9f12df9575b2
Liam Perlaki <lperlaki@icloud.com>
2018-10-11 19:29:54 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Ajay Form
+
+> Submit forms with Ajax
+
+index.html
+
+```html
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="form.js"></script>
+    <script defer src="main.js"></script>
+</head>
+<body>
+    <form id="getDog" action="https://dog.ceo/api/breeds/image/random" method="GET">
+            <button>Get Dogs</button>
+    </form>
+</body>
+</html>
+```
+
+main.js
+
+```js
+registerForm("form#getDog", async res => {
+  console.log(await res.json());
+});
+```
---
+++ src/index.js
@@ -0,0 +1,48 @@
+function formToJson(element) {
+    const form = new FormData(element)
+    const data = {}
+    for (let k of form.keys()) {
+        data[k] = form.get(k)
+    }
+    return data
+}
+
+async function submitForm(event) {
+    event.preventDefault()
+    try {
+        const method = this.method.toUpperCase()
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'text/plain,application/json; charset=utf-8'
+            },
+        }
+
+        if (method != 'GET') {
+            options.body = JSON.stringify(formToJson(this))
+        }
+
+        this.dispatchEvent(new CustomEvent('success', {
+            detail: {
+                response: await fetch(this.action, options),
+            },
+            bubbles: true,
+            cancelable: true
+        }))
+    } catch (error) {
+        this.dispatchEvent(new ErrorEvent('formSubmit', {
+            error: error,
+            message: 'Something went wrong'
+        }))
+    }
+}
+
+export default function registerForm(selector, success) {
+    const el = document.querySelector(selector)
+    el.addEventListener('submit', submitForm)
+    success && el.addEventListener('success', (e) => {
+        success(e.detail.response)
+    })
+}
+
+if (window) window.registerForm = registerForm
